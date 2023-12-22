d1b68c5385a3fe75c5bb7c8ba1e4d34dc5bdbc88
Dimitri Avtenyev <dimitri.avtenyev@gmail.com>
2023-12-22 15:56:31 +0100

    add env to handle input setEnv

---
+++ Env.cs
@@ -0,0 +1,18 @@
+﻿namespace Fx_converter
+{
+    public static class Env
+    {
+        public static void Load(string path) {
+            if(!File.Exists(path)) {
+                return;
+            }
+            foreach (var line in File.ReadAllLines(path)) {
+                var lines = line.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                if (lines.Length != 2) {
+                    continue;
+                }
+                Environment.SetEnvironmentVariable(lines[0], lines[1]);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using Fx_converter;
 
+// get cd and load env vars
+var root = Directory.GetCurrentDirectory();
+var dotenv = Path.Combine(root, ".env");
+Env.Load(dotenv);
+
 await Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webbuilder =>
     {

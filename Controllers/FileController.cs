a2df8e4138c5ca78d9f5166f469955c267ea5389
Dimitri Avtenyev <dimitri.avtenyev@gmail.com>
2023-12-23 14:12:13 +0100

    add example code for modified file

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Fx_converter.Services.ExcelProcessor;
+﻿using ClosedXML.Excel;
+using Fx_converter.Services.ExcelProcessor;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fx_converter.Controllers
@@ -18,21 +19,32 @@ namespace Fx_converter.Controllers
             if (Path.GetExtension(file.FileName) != ".xlsx") {
                 return BadRequest(new { error = $"Invalid file extension: '{Path.GetExtension(file.FileName)}'" });
             }
-            
+
             try {
-                // demo returning the same file without processing
-                using (var memoryStream = new MemoryStream()) {
-                    file.CopyTo(memoryStream);
-                    byte[] processedFile = memoryStream.ToArray();
-                    //await _excelProcessor.Process();
-                    return File(processedFile, file.ContentType, file.FileName);
+                // demo exammple using ClosedXML package
+                using (var stream = file.OpenReadStream())
+                using (var workbook = new XLWorkbook(stream)) {
+                    // Access the worksheet (assuming there is only one in this example)
+                    var worksheet = workbook.Worksheet(1);
+                    // Modify the cell (for example, change A1)
+                    worksheet.Cell("A1").Value = "Modified Value";
+                    
+                    // Save the modified content back to a MemoryStream
+                    using (var modifiedStream = new MemoryStream()) {
+                        workbook.SaveAs(modifiedStream);
+                        // Convert the MemoryStream to a byte array
+                        byte[] modifiedFile = modifiedStream.ToArray();
+
+                        // Return the modified file with the correct content type
+                        return File(modifiedFile, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", file.FileName);
+                    }
                 }
+
             } catch (Exception ex) {
                 Console.WriteLine(ex);
                 return StatusCode(500, "Internal server error");
             }
-        
-            //return File(file.ContentDisposition, file.ContentType);
+
         }
     }
 }
